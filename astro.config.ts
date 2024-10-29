b1e33cdc51dd96135169a2f944da9eba4115981c
bigsky77 <simonjudd2@gmail.com>
2024-10-29 09:43:22 +0100

    fix RSS

--- astro.config.ts
+++ astro.config.ts
@@ -50,7 +50,7 @@ export default defineConfig({
 	// https://docs.astro.build/en/guides/prefetch/
 	prefetch: true,
 	// ! Please remember to replace the following site property with your own domain
-	site: "https://psycho-virtual.chriswilliams.dev/",
+	site: "https://psycho-virtual.io/",
 	vite: {
 		optimizeDeps: {
 			exclude: ["@resvg/resvg-js"],
